f16f2003672c9ed7ebb3be66a07ab29fcce242f3
CarlosMartinezRobles <90333573+CarlosMartinezRobles@users.noreply.github.com>
2021-10-20 10:37:14 +0200

    iteracion 4-1-4 creada y cerrada

---
+++ Ejercicio4/4-1-4/index4-1-4.js
@@ -0,0 +1,4 @@
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+rickAndMortyCharacters.push ("Morty","Summer");
+let ultimo = rickAndMortyCharacters[rickAndMortyCharacters.length - 1]
+console.log (ultimo)
