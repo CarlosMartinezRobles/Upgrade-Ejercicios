99367678608669eeea97ec2d8a47e6d3e3f09cfc
CarlosMartinezRobles <90333573+CarlosMartinezRobles@users.noreply.github.com>
2021-10-20 10:55:26 +0200

    iteracion 4-1-5 creada y cerrada

---
+++ Ejercicio4/4-1-5/index4-1-5.js
@@ -0,0 +1,6 @@
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+rickAndMortyCharacters.pop("Lapiz Lopez");
+let ultimo = rickAndMortyCharacters[rickAndMortyCharacters.length - 1];
+console.log(ultimo);
+let primero= rickAndMortyCharacters[0];
+console.log(primero);
