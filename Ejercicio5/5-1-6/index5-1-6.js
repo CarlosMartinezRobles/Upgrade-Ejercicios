92672ab6251ef42447eaf3b1e90c21364a3e3aab
CarlosMartinezRobles <90333573+CarlosMartinezRobles@users.noreply.github.com>
2021-10-20 12:13:53 +0200

    iteracion 5-1-6 creada y cerrada

---
+++ Ejercicio5/5-1-6/index5-1-6.js
@@ -0,0 +1,10 @@
+const number1 = 10;
+const number2 = 20;
+const number3 = 2;
+
+if (number2/2==number1||number1/5==number3){
+    console.log('Solucionado');
+}
+else {
+    console.log('ñe, ñe, prueba otra vez');
+}
