867014cf764ed50770e79f3e398d28f6ec487904
CarlosMartinezRobles <90333573+CarlosMartinezRobles@users.noreply.github.com>
2021-10-27 21:00:47 +0200

    ejercicio 6-1-4 cerrado

--- upgrade-basic-functions/Ejercicio4/f2-1-4.js
+++ upgrade-basic-functions/Ejercicio4/f2-1-4.js
@@ -1,9 +1,12 @@
 
 const numbers = [12, 21, 38, 5, 45, 37, 6];
-function average() {
-   avg=0;
-   let sum = numbers.reduce((previous, current) => current += previous);
-   let avg = sum / numbers.length; 
-   return(numbers) 
+let Max= Math.max(...numbers);
+
+let Min= Math.min(...numbers);
+
+
+function average(promedio) {
+   promedio = (Max + Min) / 2;
+   console.log( 'El promedio de numbers es: '+promedio);
 }
 average(numbers);
