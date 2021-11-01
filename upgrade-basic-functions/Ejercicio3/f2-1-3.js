3efe2e03cb93411b0534caeb41b18206dc8a31de
CarlosMartinezRobles <90333573+CarlosMartinezRobles@users.noreply.github.com>
2021-11-01 18:07:39 +0100

    6-1-4 cerrado

--- upgrade-basic-functions/Ejercicio3/f2-1-3.js
+++ upgrade-basic-functions/Ejercicio3/f2-1-3.js
@@ -2,8 +2,11 @@ const numbers = [1, 2, 15, 5, 45, 37, 58];
 let suma=0;
 function sumaTotal (i){
    suma=i+suma;
-   console.log(i);
+  //console.log(i);
+  
 }
+
 numbers.forEach(sumaTotal)
 
 console.log(suma);
+//sumaTotal(suma);
--- upgrade-basic-functions/Ejercicio4/f2-1-4.js
+++ upgrade-basic-functions/Ejercicio4/f2-1-4.js
@@ -1,4 +1,4 @@
-
+/*
 const numbers = [12, 21, 38, 5, 45, 37, 6];
 let Max= Math.max(...numbers);
 
@@ -10,3 +10,14 @@ function average(promedio) {
    console.log( 'El promedio de numbers es: '+promedio);
 }
 average(numbers);
+
+*/
+const list = ['Raising Arizona','Fargo','Barton Fink'];
+
+addItem('The big Lewoski', titulo)
+  .then(() => addItem('O Brother, Where Art Thou?', list))
+  .then(() => addItem('The Man Who Wasnt There', list))
+  .then(() => addItem('The Ladykillers', list))
+  .then(() => {
+    console.log(list);
+  });
