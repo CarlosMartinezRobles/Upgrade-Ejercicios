8ab56a580f8ac13321aeaa79381495300c7ae633
CarlosMartinezRobles <90333573+CarlosMartinezRobles@users.noreply.github.com>
2021-11-01 20:42:56 +0100

    cambios

--- upgrade-basic-algorithms/Ejercicio2/Iteracion2-4.js
+++ upgrade-basic-algorithms/Ejercicio2/Iteracion2-4.js
@@ -8,3 +8,11 @@ const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
  car1.finalPrice= (car2.basePrice + globalBasePrice);
  console.log(car1.finalPrice);
  console.log(car2.finalPrice);
+
+ function finalPrice (coche){
+     coche.finalprice = coche.basePrice +globalBasePrice;
+     console.log(coche.finalPrice);
+ }
+
+ finalPrice(car1);
+ finalPrice(car2);
