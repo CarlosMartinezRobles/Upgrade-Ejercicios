dc2beb272ed63121c9d0f83abd12eb67c902c61d
CarlosMartinezRobles <90333573+CarlosMartinezRobles@users.noreply.github.com>
2021-11-15 13:31:00 +0100

    DOM abierto falta 1.6

--- Js DOM/Ejercicio1/DOM1-1/f1-1-1.js
+++ Js DOM/Ejercicio1/DOM1-1/f1-1-1.js
@@ -1,10 +1,26 @@
-let showme= document.querySelector('.showme')
+const showme= document.querySelector('.showme'); //1.1
 console.log(showme);
-let h1= document.querySelector('#pillado')
+//////////////////////////////////////////////////////////////////////
+const h1= document.querySelector('#pillado');//1.2
 console.log(h1);
-let p= document.querySelectorAll('p')//faltan los otros campos p
-console.log(p);
-let pokemon= document.querySelectorAll('.pokemon') 
-console.log(pokemon.length);
-let textMe= document.querySelectorAll("testMe") 
-console.log()
+/////////////////////////////////////////////////////////
+const p= document.querySelectorAll('p');//1.3
+console.log(p[0]);
+console.log(p[1]);
+console.log(p[2]);
+console.log(p[3]);//1.3
+//////////////////////////////////////////queremos hacer una funcion para hacer un loop para sacar todo s los powerRangers//////////////////
+let pokemon= document.querySelectorAll('.pokemon');//1.4
+console.log(pokemon[0]);
+console.log(pokemon[1]);
+console.log(pokemon[2]);
+console.log(pokemon[3]);//1.4
+////////////////////////////////////////////////////////////
+let textMe= document.querySelectorAll('span'); //1.5
+console.log(textMe[0]);
+console.log(textMe[1]);
+console.log(textMe[2]);
+console.log(textMe[3]);
+///////////////////////////////////////////////////////
+let textRick= document.querySelectorAll(".testMe"); //1.5
+console.log(textRick);//no se como va lo de data-selection
