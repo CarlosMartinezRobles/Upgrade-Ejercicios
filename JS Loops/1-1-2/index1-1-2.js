f61f637bc4b92c72dee2b8947f4b0f20f996294c
CarlosMartinezRobles <90333573+CarlosMartinezRobles@users.noreply.github.com>
2021-11-22 13:27:58 +0100

    jas Loops ejercicio 2 cerrado

--- JS Loops/1-1-2/index1-1-2.js
+++ JS Loops/1-1-2/index1-1-2.js
@@ -1,13 +1,29 @@
-const productos = ['Camiseta de Pokemon', 'Pantalón coquinero', 'Gorra de gansta', 'Camiseta de Basket', 'Cinrurón de Orión', 'AC/DC Camiseta']
-function buscaArray(param,loQueSeIncluye) {
-    param.forEach(
-        function (elemento) {
-            if (elemento.includes(loQueSeIncluye)){
-            console.log(elemento)
-            }
-            else{console.log('no lleva');}
-        });
+const alumnos = [
+    {name: 'Pepe Viyuela', T1: false, T2: false, T3: true}, 
+		{name: 'Lucia Aranda', T1: true, T2: false, T3: true},
+		{name: 'Juan Miranda', T1: false, T2: true, T3: true},
+		{name: 'Alfredo Blanco', T1: false, T2: false, T3: false},
+		{name: 'Raquel Benito', T1: true, T2: true, T3: true}
+]
 
+function aprobados(parametro) {
+
+    let aprobados = 0;
+       parametro.forEach((element)=> {
+            for (let indice in element){
+                if(element[indice] === true){
+                    aprobados += 1;
+                    console.log('Es true: ' + element[indice] + ' y lleva aprobados: ' + aprobados)
+                }else{
+                    console.log('Es false: ' + element[indice] + ' y lleva aprobados: ' + aprobados)
+                }
+            }
+            if ((aprobados) => 2){
+                element.isApproved = aprobados; 
+            }
+            aprobados = 0;
+        });
 }
-let camiseta = 'Camiseta';
-buscaArray(productos, camiseta);
+
+aprobados(alumnos);
+console.log(alumnos);
